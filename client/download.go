7358b7acc6295362386c9c1fa4c5a9c66b8f59af
InfectedMunshroom <h.a.ayushman@gmail.com>
2024-10-13 14:27:27 +0530

    Major: Created basic download.html GUI for downloading files Minor: Refactoring for more usability and working Images that were made are stored to ensure direct working of download Changes to be committed: 	new file:   aes/aes_encrypted_output.png 	renamed:    client/download/download.go -> client/download.go 	renamed:    client/upload/upload.go -> client/upload.go 	new file:   files/encrypted_output.png 	new file:   info/dec_encrypted_output.png 	deleted:    internal/client/client.go 	deleted:    internal/server/checkAndDecrypt.go 	new file:   steganography/aes_encrypted_output.png 	new file:   steganography/download.html 	new file:   steganography/encrypted_output.png 	modified:   steganography/main.go 	new file:   steganography/temp

--- client/download.go
+++ client/download.go
@@ -1,4 +1,4 @@
-package main
+package client
 
 import (
 	"encoding/json"
@@ -15,23 +15,23 @@ type DownloadResponse struct {
 	FileData []byte `json:"file_data"`
 }
 
-func main() {
+func DownloadFile(host, file, downloadPath string) error {
 	// Specify the URL of the server (adjust the port and URL if necessary)
-	url := "http://localhost:8081/download?file=dec_encrypted_output.png"
+	// url := "http://localhost:8081/download?file=dec_encrypted_output.png"
+	url := "http://" + host + "/download?file=" + file
 
 	// Send the HTTP GET request to download the file
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error during HTTP request:", err)
-		return
+		return fmt.Errorf("Error during HTTP request:%v", err)
+
 	}
 	defer response.Body.Close()
 
 	// Check for non-200 status code, which indicates an error from the server
 	if response.StatusCode != http.StatusOK {
 		body, _ := ioutil.ReadAll(response.Body)
-		fmt.Printf("Server returned status %d: %s\n", response.StatusCode, string(body))
-		return
+		return fmt.Errorf("Server returned status %d: %s\n", response.StatusCode, string(body))
 	}
 
 	// Read and parse the JSON response
@@ -42,14 +42,14 @@ func main() {
 		body, _ := ioutil.ReadAll(response.Body)
 		fmt.Println("Error decoding JSON response:", err)
 		fmt.Println("Raw response body:", string(body))
-		return
+		return fmt.Errorf("Error decoding JSON Response: %v", err)
 	}
 
 	// Decrypt the AES key using RSA (Assuming DecryptAES is your decryption function)
 	decryptedAESKey, err := encryptdecrypt.DecryptAES("./../final", downloadResponse.AESKey)
 	if err != nil {
-		fmt.Println("Error in decoding the AES key:", err)
-		return
+		return fmt.Errorf("Error in decoding the AES key: %v", err)
+
 	}
 	fmt.Println("Decrypted AES key:", string(decryptedAESKey))
 
@@ -57,30 +57,31 @@ func main() {
 	tempFilePath := "temp"
 	err = ioutil.WriteFile(tempFilePath, downloadResponse.FileData, 0644)
 	if err != nil {
-		fmt.Println("Error writing encrypted data to 'temp':", err)
-		return
+		return fmt.Errorf("Error writing encrypted data to 'temp':%v", err)
+
 	}
 	fmt.Println("Encrypted file data written to 'temp'.")
 
 	// Decrypt the file data using the decrypted AES key (Assuming DecodeFile is your decryption function)
 	decryptedFileData, err := encryptdecrypt.DecodeFile(decryptedAESKey, tempFilePath)
 	if err != nil {
-		fmt.Println("Error decrypting file:", err)
-		return
+		return fmt.Errorf("Error decrypting file: %v", err)
+
 	}
 
 	// Prompt the user to specify the download path
-	fmt.Print("Enter the path where you want to save the decrypted file (including file name): ")
-	var downloadPath string
-	fmt.Scan(&downloadPath)
+	// fmt.Print("Enter the path where you want to save the decrypted file (including file name): ")
+	// var downloadPath string
+	// fmt.Scan(&downloadPath)
 
 	// Write the decrypted file data to the specified location
 	cleanPath := filepath.Clean(downloadPath)
 	err = ioutil.WriteFile(cleanPath, decryptedFileData, 0644)
 	if err != nil {
-		fmt.Println("Error saving the decrypted file:", err)
-		return
+		return fmt.Errorf("Error saving the decrypted file: %v", err)
+
 	}
 
 	fmt.Println("File downloaded and decrypted successfully at:", cleanPath)
+	return nil
 }
